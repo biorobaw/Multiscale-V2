28fdd62b2455f9a9baa9a9f3627c632df47d0f0b
Pablo Scleidorovich <bucefalog@gmail.com>
2020-04-16 12:15:45 -0400

    added missing pos processing scripts

---
+++ scripts/log_processing/postProcessPlot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --time=0:10:00
+#SBATCH --cpus-per-task 2 
+#SBATCH --qos=preempt
+#SBATCH --mem=2000M
+#SBATCH -p mri2016
+#SBATCH -o ./slurm/output.%A.%a.out # STDOUT
+
+baseDir=$1
+configId=c$SLURM_ARRAY_TASK_ID
+
+echo "configId " $configId
+
+python plotConfig.py $baseDir $configId
+
