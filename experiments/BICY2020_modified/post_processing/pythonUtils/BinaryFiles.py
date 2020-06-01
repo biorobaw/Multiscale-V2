40395cf81d9b1a05a2f40d49cef440b475019838
Pablo Scleidorovich <bucefalog@gmail.com>
2020-06-01 16:03:04 -0400

    modified gitignore, and added files to new set of experiments BICY2020_modified

---
+++ experiments/BICY2020_modified/post_processing/pythonUtils/BinaryFiles.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+# this file assumes data was encoded using little endian
+
+
+def load_int_vector(file):
+    file_is_str = type(file) is str
+    if file_is_str:
+        file = open(file, 'rb')
+    length = read_vector_size(file)
+    result = np.fromfile(file, dtype=np.uint32, count=length)
+
+    if file_is_str:
+        file.close()
+    return result
+
+
+def read_vector_size(file):
+    file_is_str = type(file) is str
+    if file_is_str:
+        file = open(file, 'rb')
+    result = np.fromfile(file, dtype=np.uint32, count=1)
+    if file_is_str:
+        file.close()
+    return result.item()
+
+
+def load_float_matrix(file):
+    file_is_str = type(file) is str
+    if file_is_str:
+        file = open(file, 'rb')
+    lengths = np.fromfile(file, dtype=np.uint32, count=2)
+    total = lengths[0]*lengths[1]
+    result = np.fromfile(file, dtype=np.float32, count=total).reshape((lengths[0], -1))
+    if file_is_str:
+        file.close()
+    return result
--- experiments/BICY2020_modified/run_plot_all_experiments.sh
+++ experiments/BICY2020_modified/run_plot_all_experiments.sh
@@ -1,12 +1,12 @@
 #in git root folder execute the following commands:
 
-RUN_ALL="scripts/log_processing/plotArticleFigures.py"
+PLOT_EXPERIMENT="experiments/BICY2020_modified/post_processing/plot_experiments.py"
 LOG_FOLDER="experiments/BICY2020_modified/logs/"
 module add apps/python/3.7.3
 
-python $RUN_ALL ${LOG_FOLDER}experiment1-traces/
-# python $RUN_ALL ${LOG_FOLDER}experiment2-singleMin/
-# python $RUN_ALL ${LOG_FOLDER}experiment3-singleSame/
-# python $RUN_ALL ${LOG_FOLDER}experiment4-mazes/
-# python $RUN_ALL ${LOG_FOLDER}experiment5-single/
-# python $RUN_ALL ${LOG_FOLDER}experiment5-twoScales/
+python $PLOT_EXPERIMENT ${LOG_FOLDER}experiment1-traces/
+# python $PLOT_EXPERIMENT ${LOG_FOLDER}experiment2-singleMin/
+# python $PLOT_EXPERIMENT ${LOG_FOLDER}experiment3-singleSame/
+# python $PLOT_EXPERIMENT ${LOG_FOLDER}experiment4-mazes/
+# python $PLOT_EXPERIMENT ${LOG_FOLDER}experiment5-single/
+# python $PLOT_EXPERIMENT ${LOG_FOLDER}experiment5-twoScales/
