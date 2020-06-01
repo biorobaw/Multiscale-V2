facf349be35f3db60916f430c9d043f56d1571bb
Pablo Scleidorovich <bucefalog@gmail.com>
2020-06-01 16:14:17 -0400

    modified scripts to run in the cluster

--- experiments/BICY2020_Multiscale_memory_experiments/run_all.sh
+++ experiments/BICY2020_Multiscale_memory_experiments/run_all.sh
@@ -6,7 +6,7 @@ LOG_FOLDER="logs/BICY2020_redone/"
 
 sh $RUN_ALL ${EXPERIMENT_FOLDER}/experiment1-Traces.csv ${LOG_FOLDER}experiment1-traces 100
 sh $RUN_ALL ${EXPERIMENT_FOLDER}/experiment2-SingleMin.csv ${LOG_FOLDER}experiment2-singleMin 100
-sh $RUN_ALL ${EXPERIMENT_FOLDER}/experiment3-SingleSame.csv ${LOG_FOLDER}experiment3-singleSame 100
+sh $RUN_ALL ${EXPERIMENT_FOLDER}/experiment3-SingleSame.csv ${LOG_FOLDER}experiment3-singleSame 20
 sh $RUN_ALL ${EXPERIMENT_FOLDER}/experiment4-Mazes.csv ${LOG_FOLDER}experiment4-mazes 100
 sh $RUN_ALL ${EXPERIMENT_FOLDER}/experiment5-Single.csv ${LOG_FOLDER}experiment5-single 100
 sh $RUN_ALL ${EXPERIMENT_FOLDER}/experiment5-TwoScales.csv ${LOG_FOLDER}experiment5-twoScales 100
