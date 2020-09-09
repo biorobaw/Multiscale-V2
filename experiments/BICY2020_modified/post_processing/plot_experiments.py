7a5a58ea948bfd90533d3427ac2f2042bd661b9d
Pablo Scleidorovich <bucefalog@gmail.com>
2020-09-09 16:23:18 -0400

    Modified plotting script so that plotting of experiments 1 and 5 are equal

--- experiments/BICY2020_modified/post_processing/plot_experiments.py
+++ experiments/BICY2020_modified/post_processing/plot_experiments.py
@@ -17,38 +17,7 @@ def make_folder(folder):
         os.makedirs(folder)
 
 
-def plot_experiment_traces(figure_folder, configs, sample_rate, db):
-    """ for each scale, grouping by trace value:
-            plot runtimes
-            box plot of last episode
-            dunn test of last episode
-    """
-
-    # PARAMETERS:
-    location = -1  # we will only plot geometric mean data (represented with location -1)
-    num_episodes = configs['numEpisodes'].max() / configs['numStartingPositions'].max()
-    last_episode = -sample_rate % num_episodes
-
-    # plot each scale separately
-    for scale, sub_configs in configs.groupby(['pcSizes']):
-        # get indices and format text for the plots
-        scale_str = "{0:.2f}".format(scale)  # convert to string and format
-        print('plotting scale: ', scale)
-
-        # plot titles and legends:
-
-        group_name    = f's{scale_str}'
-        legend_title  = 'Trace'
-        legend_values = sub_configs.traces.map("{0:.2f}".format)
-        plot_title    = f"Scale {scale_str}"
-
-        plot_runtimes_boxplots_dunntest(db, sub_configs, location, last_episode, group_name,
-                                    legend_title, legend_values, plot_title, figure_folder)
-
-        plot_deltaV(db, sub_configs, location, last_episode, group_name,
-                                    legend_title, legend_values, plot_title, figure_folder)
-
-def plot_experiment_5_density(figure_folder, configs, sample_rate, db):
+def plot_experiment_traces_and_scales_per_maze(figure_folder, configs, sample_rate, db):
     """ for each scale, grouping by trace value:
             plot runtimes
             box plot of last episode
@@ -98,7 +67,7 @@ def plot_experiment_5_density(figure_folder, configs, sample_rate, db):
 
             group_name = f't{trace:.1f}'
             legend_title = 'Columns'
-            legend_values = sub_configs.numX.map("{0:.2f}".format)
+            legend_values = sub_configs.numX.map("{}".format)
             plot_title = f"Maze {maze} - Trace {trace:.1f}"
 
             plot_runtimes_boxplots_dunntest(db, sub_configs, location, last_episode, group_name,
@@ -106,6 +75,7 @@ def plot_experiment_5_density(figure_folder, configs, sample_rate, db):
 
             plot_deltaV(db, sub_configs, location, last_episode, group_name,
                         legend_title, legend_values, plot_title, maze_figure_folder)
+
 
 def plot_scale_experiment(figure_folder, configs, sample_rate, db):
     """
@@ -194,11 +164,11 @@ def plot_experiment(folder):
                             .split(sep='-')[0][10:]  # all experiments use syntax 'experimentN-...'
 
     experiment_map = {}
-    experiment_map['1'] = ( plot_experiment_traces,         5 )
-    experiment_map['2'] = ( plot_scale_experiment,          5 )
-    experiment_map['3'] = ( plot_scale_experiment,          5 )
-    experiment_map['4'] = ( plot_experiment4_extraAtFeeder, 5 )
-    experiment_map['5'] = (plot_experiment_5_density      , 10)
+    experiment_map['1'] = ( plot_experiment_traces_and_scales_per_maze, 5 )
+    experiment_map['2'] = ( plot_scale_experiment                     , 5 )
+    experiment_map['3'] = ( plot_scale_experiment                     , 5 )
+    experiment_map['4'] = ( plot_experiment4_extraAtFeeder            , 5 )
+    experiment_map['5'] = ( plot_experiment_traces_and_scales_per_maze, 10)
 
     # plot the experiment
     e = experiment_map[experiment_name]
