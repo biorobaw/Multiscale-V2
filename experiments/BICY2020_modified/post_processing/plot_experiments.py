86bbea57ada7f0fc3b4b9f70806cc5958fa1d098
Pablo Scleidorovich <bucefalog@gmail.com>
2020-09-09 18:21:41 -0400

    Modified plotting script so that plotting of experiments 1 and 5 are equal

--- experiments/BICY2020_modified/post_processing/plot_experiments.py
+++ experiments/BICY2020_modified/post_processing/plot_experiments.py
@@ -39,6 +39,65 @@ def plot_experiment_traces_and_scales_per_maze(figure_folder, configs, sample_ra
         make_folder(maze_figure_folder)
 
         # plot each scale separately
+        for r, sub_configs in maze_configs.groupby(['pcSizes']):
+            # get indices and format text for the plots
+            # nx_str = "{0:.2f}".format(scale)  # convert to string and format
+            print('plotting r: ', r)
+
+            # plot titles and legends:
+
+            group_name    = f's{r:.2f}'
+            legend_title  = 'Traces'
+            legend_values = sub_configs.traces.map("{0:.1f}".format)
+            plot_title    = f"Maze {maze} - PC radius {int(r*100)}cm"
+
+            plot_runtimes_boxplots_dunntest(db, sub_configs, location, last_episode, group_name,
+                                        legend_title, legend_values, plot_title, maze_figure_folder)
+
+            plot_deltaV(db, sub_configs, location, last_episode, group_name,
+                                        legend_title, legend_values, plot_title, maze_figure_folder)
+
+        # plot each scale separately
+        for trace, sub_configs in maze_configs.groupby(['traces']):
+            # get indices and format text for the plots
+            # nx_str = "{0:.2f}".format(scale)  # convert to string and format
+            print('plotting trace: ', trace)
+
+            # plot titles and legends:
+
+            group_name = f't{trace:.1f}'
+            legend_title = 'Columns'
+            legend_values = sub_configs.numX.map("{}".format)
+            plot_title = f"Maze {maze} - Trace {trace:.1f}"
+
+            plot_runtimes_boxplots_dunntest(db, sub_configs, location, last_episode, group_name,
+                                            legend_title, legend_values, plot_title, maze_figure_folder)
+
+            plot_deltaV(db, sub_configs, location, last_episode, group_name,
+                        legend_title, legend_values, plot_title, maze_figure_folder)
+
+def plot_experiment_traces_and_nx_per_maze(figure_folder, configs, sample_rate, db):
+    """ for each scale, grouping by trace value:
+            plot runtimes
+            box plot of last episode
+            dunn test of last episode
+    """
+
+    # PARAMETERS:
+    location = -1  # we will only plot geometric mean data (represented with location -1)
+    num_episodes = configs['numEpisodes'].max() / configs['numStartingPositions'].max()
+    last_episode = -sample_rate % num_episodes
+
+    # for each maze
+    for maze, maze_configs in configs.groupby(['mazeFile']):
+
+        maze = ntpath.basename(maze)[1:-4]
+        print('plotting maze: ', maze)
+
+        maze_figure_folder = figure_folder + 'M' + maze +'/'
+        make_folder(maze_figure_folder)
+
+        # plot each scale separately
         for nx, sub_configs in maze_configs.groupby(['numX']):
             # get indices and format text for the plots
             # nx_str = "{0:.2f}".format(scale)  # convert to string and format
@@ -75,7 +134,6 @@ def plot_experiment_traces_and_scales_per_maze(figure_folder, configs, sample_ra
 
             plot_deltaV(db, sub_configs, location, last_episode, group_name,
                         legend_title, legend_values, plot_title, maze_figure_folder)
-
 
 def plot_scale_experiment(figure_folder, configs, sample_rate, db):
     """
@@ -168,7 +226,7 @@ def plot_experiment(folder):
     experiment_map['2'] = ( plot_scale_experiment                     , 5 )
     experiment_map['3'] = ( plot_scale_experiment                     , 5 )
     experiment_map['4'] = ( plot_experiment4_extraAtFeeder            , 5 )
-    experiment_map['5'] = ( plot_experiment_traces_and_scales_per_maze, 10)
+    experiment_map['5'] = ( plot_experiment_traces_and_nx_per_maze    , 10)
 
     # plot the experiment
     e = experiment_map[experiment_name]
