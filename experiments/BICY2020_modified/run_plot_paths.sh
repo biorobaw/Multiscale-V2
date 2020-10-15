99a412468970c9c03bc6e30b47a8aa5e1cd5b787
Pablo Scleidorovich <bucefalog@gmail.com>
2020-10-15 11:03:37 -0400

    Added missing keyword '--wrap' to path plotting slurm script

--- experiments/BICY2020_modified/run_plot_paths.sh
+++ experiments/BICY2020_modified/run_plot_paths.sh
@@ -6,9 +6,9 @@ PLOT_EXPERIMENT=$EXPERIMENT_FOLDER/post_processing/plot_paths.py
 
 BATCH_SCRIPT="module add apps/python/3.7.3; python $PLOT_EXPERIMENT $LOG_FOLDER/ENAME/ c\$SLURM_ARRAY_TASK_ID"
 
-[ -z "$DO_EXPERIMENT_1" ] || sbatch -a 0-279 --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment1-traces}"
-[ -z "$DO_EXPERIMENT_2" ] || sbatch -a 0-27  --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment2-singleMin}"
-[ -z "$DO_EXPERIMENT_3" ] || sbatch -a 0-27  --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment3-singleSame}"
-[ -z "$DO_EXPERIMENT_4" ] || sbatch -a 0-219 --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment4-extraAtFeeder}"
-[ -z "$DO_EXPERIMENT_5" ] || sbatch -a 0-159 --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment5-density}"
-[ -z "$DO_EXPERIMENT_6" ] || sbatch -a 0-109 --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment6-extraAtGap}"
+[ -z "$DO_EXPERIMENT_1" ] || sbatch -a 0-279 --mem=500M --time=0:15:00 --cpus-per-task=2 --wrap="${BATCH_SCRIPT/ENAME/experiment1-traces}"
+[ -z "$DO_EXPERIMENT_2" ] || sbatch -a 0-27  --mem=500M --time=0:15:00 --cpus-per-task=2 --wrap="${BATCH_SCRIPT/ENAME/experiment2-singleMin}"
+[ -z "$DO_EXPERIMENT_3" ] || sbatch -a 0-27  --mem=500M --time=0:15:00 --cpus-per-task=2 --wrap="${BATCH_SCRIPT/ENAME/experiment3-singleSame}"
+[ -z "$DO_EXPERIMENT_4" ] || sbatch -a 0-219 --mem=500M --time=0:15:00 --cpus-per-task=2 --wrap="${BATCH_SCRIPT/ENAME/experiment4-extraAtFeeder}"
+[ -z "$DO_EXPERIMENT_5" ] || sbatch -a 0-159 --mem=500M --time=0:15:00 --cpus-per-task=2 --wrap="${BATCH_SCRIPT/ENAME/experiment5-density}"
+[ -z "$DO_EXPERIMENT_6" ] || sbatch -a 0-109 --mem=500M --time=0:15:00 --cpus-per-task=2 --wrap="${BATCH_SCRIPT/ENAME/experiment6-extraAtGap}"
