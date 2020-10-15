f3843901b8baebacc8fbdddbc74b039078b7945d
Pablo Scleidorovich <bucefalog@gmail.com>
2020-10-15 11:01:02 -0400

    Fixed last commit which modified an incorrect file

--- experiments/BICY2020_modified/run_plot_paths.sh
+++ experiments/BICY2020_modified/run_plot_paths.sh
@@ -4,13 +4,11 @@ source "experiments/BICY2020_modified/run_set_variables.sh"
 
 PLOT_EXPERIMENT=$EXPERIMENT_FOLDER/post_processing/plot_paths.py
 
-module add apps/python/3.7.3
-[ -z "$DO_EXPERIMENT_1" ] || python $PLOT_EXPERIMENT $LOG_FOLDER/experiment1-traces/
-[ -z "$DO_EXPERIMENT_2" ] || python $PLOT_EXPERIMENT $LOG_FOLDER/experiment2-singleMin/
-[ -z "$DO_EXPERIMENT_3" ] || python $PLOT_EXPERIMENT $LOG_FOLDER/experiment3-singleSame/
-[ -z "$DO_EXPERIMENT_4" ] || python $PLOT_EXPERIMENT $LOG_FOLDER/experiment4-extraAtFeeder/
-[ -z "$DO_EXPERIMENT_5" ] || python $PLOT_EXPERIMENT $LOG_FOLDER/experiment5-density/
-[ -z "$DO_EXPERIMENT_6" ] || python $PLOT_EXPERIMENT $LOG_FOLDER/experiment6-extraAtGap/
-# python $PLOT_EXPERIMENT ${LOG_FOLDER}experiment4-mazes/
-# python $PLOT_EXPERIMENT ${LOG_FOLDER}experiment5-single/
-# python $PLOT_EXPERIMENT ${LOG_FOLDER}experiment5-twoScales/
+BATCH_SCRIPT="module add apps/python/3.7.3; python $PLOT_EXPERIMENT $LOG_FOLDER/ENAME/ c\$SLURM_ARRAY_TASK_ID"
+
+[ -z "$DO_EXPERIMENT_1" ] || sbatch -a 0-279 --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment1-traces}"
+[ -z "$DO_EXPERIMENT_2" ] || sbatch -a 0-27  --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment2-singleMin}"
+[ -z "$DO_EXPERIMENT_3" ] || sbatch -a 0-27  --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment3-singleSame}"
+[ -z "$DO_EXPERIMENT_4" ] || sbatch -a 0-219 --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment4-extraAtFeeder}"
+[ -z "$DO_EXPERIMENT_5" ] || sbatch -a 0-159 --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment5-density}"
+[ -z "$DO_EXPERIMENT_6" ] || sbatch -a 0-109 --mem=500M --time=0:15:00 --cpus-per-task=2 "${BATCH_SCRIPT/ENAME/experiment6-extraAtGap}"
