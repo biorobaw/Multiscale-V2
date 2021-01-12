fe36a87e4e1fa96be64957454674b73e09edd8b7
Pablo Scleidorovich <bucefalog@gmail.com>
2021-01-12 13:36:07 -0500

    Fixed bug in running scripts that required updating script names.

--- experiments/BICY2020_modified/run_4_merge_configs.sh
+++ experiments/BICY2020_modified/run_4_merge_configs.sh
@@ -4,11 +4,11 @@ source "experiments/BICY2020_modified/run_set_variables.sh"
 
 
 module add apps/python/3.7.3
-[ -z "$DO_EXPERIMENT_1" ] || python $SCRIPT_MERGE $LOG_FOLDER_E1/
-[ -z "$DO_EXPERIMENT_2" ] || python $SCRIPT_MERGE $LOG_FOLDER_E2/
-[ -z "$DO_EXPERIMENT_3" ] || python $SCRIPT_MERGE $LOG_FOLDER_E3/
-[ -z "$DO_EXPERIMENT_4" ] || python $SCRIPT_MERGE $LOG_FOLDER_E4/
-[ -z "$DO_EXPERIMENT_5" ] || python $SCRIPT_MERGE $LOG_FOLDER_E5/
-[ -z "$DO_EXPERIMENT_6" ] || python $SCRIPT_MERGE $LOG_FOLDER_E6/
-[ -z "$DO_EXPERIMENT_7" ] || python $SCRIPT_MERGE $LOG_FOLDER_E7/
+[ -z "$DO_EXPERIMENT_1" ] || python $SCRIPT_4_MERGE $LOG_FOLDER_E1/
+[ -z "$DO_EXPERIMENT_2" ] || python $SCRIPT_4_MERGE $LOG_FOLDER_E2/
+[ -z "$DO_EXPERIMENT_3" ] || python $SCRIPT_4_MERGE $LOG_FOLDER_E3/
+[ -z "$DO_EXPERIMENT_4" ] || python $SCRIPT_4_MERGE $LOG_FOLDER_E4/
+[ -z "$DO_EXPERIMENT_5" ] || python $SCRIPT_4_MERGE $LOG_FOLDER_E5/
+[ -z "$DO_EXPERIMENT_6" ] || python $SCRIPT_4_MERGE $LOG_FOLDER_E6/
+[ -z "$DO_EXPERIMENT_7" ] || python $SCRIPT_4_MERGE $LOG_FOLDER_E7/
 
