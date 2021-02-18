2dd19226e8f7599bf5ee5c57cee4f923e747f652
Pablo Scleidorovich <bucefalog@gmail.com>
2021-02-18 09:12:44 -0500

    Added path plotting ability to experiment 8

--- experiments/BICY2020_modified/post_processing/plot_paths.py
+++ experiments/BICY2020_modified/post_processing/plot_paths.py
@@ -61,7 +61,8 @@ def plot_config(experiment_folder, config, config_id):
         '4': [format_maze, lambda c: f's{c["pcSizes"]}', lambda c: f't{c["traces"]}'],
         '5': [format_maze, format_trace, format_nx],
         '6': [format_maze, lambda c: f's{c["pcSizes"]}', lambda c: f't{c["traces"]}'],
-        '7': [format_maze, format_pc_file, lambda c: f't{c["traces"]}']
+        '7': [format_maze, format_pc_file, lambda c: f't{c["traces"]}'],
+        '8': [format_maze ],
     }
     config_title = apply_formatters(config, e_formatters[experiment_name])
     save_name = os.path.join(figure_folder, 'paths_' + config_title + '.pdf')
