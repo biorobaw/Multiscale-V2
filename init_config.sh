15c582a45dd6ef37e721f5f5e9750b1cd7958ef6
Pablo Scleidorovich <bucefalog@gmail.com>
2021-06-27 11:25:16 -0400

    updated project configurations script

--- init_config.sh
+++ init_config.sh
@@ -1,2 +1,2 @@
 git clone git@github.com:biorobaw/scs.git ../
-git update-index --skip-worktree .idea/runConfigurations/*
+git update-index --skip-worktree .idea/runConfigurations/Multiscale_testing.xml
