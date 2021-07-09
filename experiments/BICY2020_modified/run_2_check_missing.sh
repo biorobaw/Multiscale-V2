25509e4aee848e555a84c8053d35af86e2aa5150
Pablo Scleidorovich <bucefalog@gmail.com>
2021-07-09 09:43:24 -0400

    updated scripts used to check for missing rats

--- experiments/BICY2020_modified/run_2_check_missing.sh
+++ experiments/BICY2020_modified/run_2_check_missing.sh
@@ -6,6 +6,6 @@ source "experiments/BICY2020_modified/run_set_variables.sh"
 CHECK_FILE="r#ID-steps.bin"
 
 for E in ${RUN[*]}; do
-	echo "sh python $SCRIPT_2_CHECK $(map $E LOG_FOLDER) $CHECK_FILE $(map $E RATS)"
-	python $SCRIPT_2_CHECK $(map $E LOG_FOLDER) $CHECK_FILE $(map $E RATS)
+	echo "sh python $SCRIPT_2_CHECK $(map $E LOG_FOLDER) $CHECK_FILE"
+	python $SCRIPT_2_CHECK $(map $E LOG_FOLDER) $CHECK_FILE
 done
